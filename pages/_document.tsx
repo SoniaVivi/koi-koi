2a63123597ff82affc2c4a4911abc3b2c38b713d
Siona Vivian <SVivn@proton.me>
2023-07-04 18:51:05 -0700

    UI: adjust mobile zoom

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -7,7 +7,7 @@ export default function Document() {
         <meta
           name="viewport"
           content="width=device-width, initial-scale=1,
- minimum-scale=1"
+ minimum-scale=1, user-scalable=yes"
         ></meta>
       </Head>
       <body>
